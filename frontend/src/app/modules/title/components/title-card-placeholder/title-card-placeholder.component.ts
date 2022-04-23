5d83bad130efd7ea61c3d8bd590ec63076b7c915
Dmitriy Arapov <darapov@dalet.com>
2022-04-23 17:32:54 +0300

    Placeholders when loading

--- frontend/src/app/modules/title/components/title-card-placeholder/title-card-placeholder.component.ts
+++ frontend/src/app/modules/title/components/title-card-placeholder/title-card-placeholder.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: `app-title-card-placeholder`,
   templateUrl: `./title-card-placeholder.component.html`,
-  styleUrls: [`./title-card-placeholder.component.scss`],
+  styleUrls: [`../title-card/title-card.component.scss`],
 })
 export class TitleCardPlaceholderComponent {
 
