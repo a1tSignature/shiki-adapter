23a7252336c48b8165a73ff8af5b87f0f8a0be17
Dmitriy Arapov <darapov@dalet.com>
2022-05-22 20:03:12 +0300

    lists

--- frontend/src/app/modules/list/list.module.ts
+++ frontend/src/app/modules/list/list.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SelectListComponent } from './components/select-list/select-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SelectListComponent,
+  ],
   imports: [
     CommonModule,
+  ],
+  exports: [
+    SelectListComponent,
   ],
 })
 export class ListModule {}
